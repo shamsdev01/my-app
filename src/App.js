90e815f1ce62d26881cd5cbf9966dcff974aa5ad
ajasa shamsuedeen <shamsudeenajasa@gmail.com>
2023-11-15 13:24:49 +0000

    agency-landing-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { ReactDOM } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import Services from './components/pages/Services'
+import Products from './components/pages/Products'
+import SignUp from './components/pages/SignUp'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/services" element={<Services/>}></Route>
+        <Route path="/Products" element={<Products/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
+     
+
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
